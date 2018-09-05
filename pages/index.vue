9180ef6835a6a397aad817af3e3cfd5ca987e9c3
soichiro-nitta <soic1226@gmail.com>
2018-09-05 18:08:46 +0900

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,8 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        20th
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="index">
+    This is index.vue<br>
+    これはトップページです
+  </div>
 </template>
 
 <script>
@@ -32,34 +15,13 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
+<style lang='scss' scoped>
+.index {
   display: flex;
-  justify-content: center;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  color: #555;
 }
 </style>
-
