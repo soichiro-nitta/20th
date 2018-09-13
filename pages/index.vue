d628c13c5d5f7ae0c43370b514919f90f364ed5e
soichiro-nitta <soic1226@gmail.com>
2018-09-13 17:27:09 +0900

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="index">
     <BaseHeading1 text="Home."/>
+    <a href="https://media.housecom.jp/wp-content/uploads/videos/180115-koenji.mp4">
+      this is video link?
+    </a>
+    <!-- <video
+      crossorigin="anonymous"
+      preload="auto"
+      x-webkit-airplay=""
+      controls=""
+      data-original-aria-hidden=""
+      src="https://media.housecom.jp/wp-content/uploads/videos/180115-koenji.mp4"
+    >
+      <source src="https://media.housecom.jp/wp-content/uploads/videos/180115-koenji.mp4">
+    </video> -->
+    <video
+      poster="https://media.housecom.jp/wp-content/uploads/2018/01/26942352_796053807253941_538403009_o-1080x459.jpg"
+      autobuffer=""
+      controls=""
+    >
+      <source src="https://media.housecom.jp/wp-content/uploads/videos/180115-koenji.mp4">
+    </video>
     <!-- <video
       ref="video"
       src="https://media.housecom.jp/wp-content/uploads/videos/180115-koenji.mp4"
       controls
-      preload="auto"
-      autoplay
-      playsinline
     /> -->
     <div class="text">OP後はこのページにきて、ボタン押して動画再生?</div>
   </div>
