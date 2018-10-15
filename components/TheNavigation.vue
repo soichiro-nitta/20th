b7573d266da0f48dd7358da31ed42c715520dbc3
soichiro-nitta <soic1226@gmail.com>
2018-10-15 16:19:49 +0900

    wip

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -79,7 +79,7 @@ export default {
   },
   watch: {
     async opening() {
-      await this.$delay(1800)
+      await this.$delay(2000)
       requestAnimationFrame(() => {
         TweenMax.to(this.$refs.switchDummy3, 0.5, {
           scale: 1,
